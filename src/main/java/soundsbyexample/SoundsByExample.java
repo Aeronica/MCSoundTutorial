fd76153e0eaadaed17d147a06d7177d3a2cfa652
Aeronica <Aeronica@users.noreply.github.com>
2016-09-10 18:40:13 -0500

    Fixing setup

---
+++ src/main/java/soundsbyexample/SoundsByExample.java
@@ -0,0 +1,16 @@
+/*
+** User: Paul Boese aka Aeronica
+** Date: Sep 8, 2016
+**
+*/
+package soundsbyexample;
+
+public class SoundsByExample
+{
+
+    public SoundsByExample()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+}
